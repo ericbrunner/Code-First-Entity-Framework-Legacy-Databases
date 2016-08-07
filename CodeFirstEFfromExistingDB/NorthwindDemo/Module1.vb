356b23baf4c3578a662a2fb97f878c13e39bcdfa
erich brunner <erich.brunner@rolsped.com>
2016-08-07 09:05:28 +0200

    added code for initial dbContext test

--- CodeFirstEFfromExistingDB/NorthwindDemo/Module1.vb
+++ CodeFirstEFfromExistingDB/NorthwindDemo/Module1.vb
@@ -1,7 +1,29 @@
-﻿Module Module1
+﻿Imports Northwind.Datalayer
+
+Module Module1
 
     Sub Main()
 
+        Using dbContext As New NorthwindDbContext()
+
+            Dim categories = dbContext.Categories
+
+            For Each category In categories
+                Console.WriteLine(category.CategoryId & " " & category.CategoryName)
+            Next
+
+            Console.WriteLine()
+
+            Dim customerHistories = dbContext.CustOrderHist("ALFKI")
+
+            For Each customerHistory In customerHistories
+                Console.WriteLine($"{customerHistory.Total, 2} {customerHistory.ProductName}")
+            Next
+
+        End Using
+
+        Console.WriteLine("Press any key to exit....")
+        Console.ReadKey()
     End Sub
 
 End Module
